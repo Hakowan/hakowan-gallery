f0532bfc0c7c0fba8be448c09eb0405f06029f3a
Qingnan Zhou <qzhou@adobe.com>
2023-12-16 13:59:42 -0500

    Update IPC result.

--- gallery/IPC/ipc.py
+++ gallery/IPC/ipc.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 import hakowan as hkw
-import lagrange
 
 # Step 1:
 # Create a ball layer containing the ball geometry.
 # Approximate the original IPC figure with pinkish material.
+
 ball = hkw.layer("data/7.obj").channel(
     material=hkw.material.RoughPlastic("salmon", alpha=0.02)
 )
@@ -14,17 +14,10 @@ ball = hkw.layer("data/7.obj").channel(
 # Create a plate layer containing the collision plate.
 # Use glass-like material so one can see the collision-induced deformation clearly.
 
-# The plate geometry has very skinny triangles. Mitsuba has trouble computing accurate normal for
-# this mesh. Thus, we explicitly generates the correct normal and use it in the normal channel.
-plate_mesh = lagrange.io.load_mesh("data/plate.obj")
-normal_id = lagrange.compute_normal(plate_mesh)
-normal_name = plate_mesh.get_attribute_name(normal_id)
-
-plate = hkw.layer(plate_mesh).channel(
-    normal=normal_name, material=hkw.material.ThinDielectric()
-)
+plate = hkw.layer("data/plate.obj").channel(material=hkw.material.ThinDielectric())
 
 # Step 3: Adjust configuration.
+
 config = hkw.config()
 # Use orthographic camera for better visualization of the collision.
 config.sensor = hkw.setup.sensor.Orthographic()
@@ -32,9 +25,18 @@ config.sensor = hkw.setup.sensor.Orthographic()
 config.integrator = hkw.setup.integrator.VolPath()
 
 # Step 4: Render!
+
 # The side view shows the ball-plate collision from the side.
-hkw.render(ball + plate, config, filename="results/ipc_side.png")
+side_view = ball + plate
+hkw.render(side_view, config, filename="results/ipc_side.png")
 
 # The back view shows the ball-plate collision from behind the plate.
-config.sensor.location = [5, 0, 0]
-hkw.render(ball + plate, config, filename="results/ipc_back.png")
+
+# Rotation matrix to rotate around y-axis by 90 degrees.
+rotate_y90 = [
+    [0, 0, -1],
+    [0, 1, 0],
+    [1, 0, 0],
+]
+back_view = (ball + plate).transform(hkw.transform.Affine(rotate_y90))
+hkw.render(back_view, config, filename="results/ipc_back.png")
