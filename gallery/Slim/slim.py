ab256a60727fb4865e6534c80b8dbcf465814e00
Qingnan Zhou <qzhou@adobe.com>
2023-12-16 22:48:34 -0500

    Simply slim code.

--- gallery/Slim/slim.py
+++ gallery/Slim/slim.py
@@ -2,8 +2,7 @@
 
 import hakowan as hkw
 import pathlib
-import scipy
-import numpy as np
+import math
 
 # Generate base layer setting.
 # We are using image-based texture for color in this example.
@@ -19,28 +18,26 @@ base = hkw.layer().channel(
 )
 
 # Figure 3
-fig3 = base.data("data/fig3.obj")
+fig3 = base.data("data/fig3.obj").rotate(axis=[0, 1, 0], angle=math.pi)
 
-# Minor adjustment of the camera and light.
+# Move the camera position closer.
 config = hkw.config()
-config.sensor.location = (0, 0, -4)
-config.emitters[0].rotation = 0
+config.sensor.location = [0, 0, 3]
 
+# Render!
 hkw.render(fig3, config, filename="results/fig3.png")
 
 # Figure 10
 
 # The input mesh comes at an odd orientation.
-# We apply affine transform to fix the orientation issue.
-M = np.eye(4, dtype=np.float64)
-M[:3, :3] = scipy.spatial.transform.Rotation.from_euler(
-    "x", -45, degrees=True
-).as_matrix()
-fig10 = base.data("data/fig10_uniform.obj").transform(hkw.transform.Affine(M))
+# We apply two rotations to fix the orientation issue.
+# First, we rotate the mesh around the x-axis by -45 degrees.
+# Then, we rotate the mesh around the y-axis by 135 degrees.
+fig10 = (
+    base.data("data/fig10_uniform.obj")
+    .rotate(axis=[1, 0, 0], angle=-math.pi / 4)
+    .rotate(axis=[0, 1, 0], angle=3 * math.pi / 4)
+)
 
-# Again, fine tuning the camera and light.
-config.y_up()
-config.sensor.location = (-3, 0, -3)
-config.emitters[0].rotation = 0
-
+# Render!
 hkw.render(fig10, config, filename="results/fig10.png")
