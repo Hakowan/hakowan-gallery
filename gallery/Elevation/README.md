436e25b445754d2ad7859bba654faac779240c21
Qingnan Zhou <qzhou@adobe.com>
2023-12-24 11:33:54 -0500

    Update and add README.

---
+++ gallery/Elevation/README.md
@@ -0,0 +1,6 @@
+# Topogrpahic Map
+
+This example aims to generate a 3D topographic map from elevation data provided by [USGS](https://www.usgs.gov/the-national-map-data-delivery/gis-data-download).
+
+## Data
+The [input](https://github.com/qnzhou/hakowan-gallery/blob/main/gallery/Elevation/data/USGS_1_n35w112.tif) is height field image representing a tile of the US elevation map (N35 and W112) downloaded from USGS. It converted to a dense quad mesh using [this script](https://github.com/qnzhou/hakowan-gallery/blob/main/gallery/Elevation/image2mesh.py).
--- gallery/Elevation/elevation.py
+++ gallery/Elevation/elevation.py
@@ -4,8 +4,11 @@ import hakowan as hkw
 import lagrange
 from image2mesh import image2mesh
 
+# Step 1: Generate a mesh from a heightmap.
 usgs_data = image2mesh("data/USGS_1_n35w112.tif")
 
+# Step 2: Create a map layer that maps elevation to color.
+# Note that we use a custom colormap here.
 usgs_map = (
     hkw.layer(usgs_data)
     .channel(
@@ -18,9 +21,10 @@ usgs_map = (
     .transform(hkw.transform.Compute(z="elevation"))
 )
 
+# Step 3: Render from two different angles.
 config = hkw.config()
 config.sensor.location = [0, 0, 3]
-#hkw.render(usgs_map, config, filename="results/usgs_1_n35112.png")
+hkw.render(usgs_map, config, filename="results/usgs_1_n35112.png")
 
 config.sensor.location = [0, -2, 2]
 hkw.render(usgs_map, config, filename="results/usgs_1_n35112_side.png")
