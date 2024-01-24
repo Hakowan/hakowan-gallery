f915132bad1d65e372f7ca046d853dc13c3416a8
Qingnan Zhou <qzhou@adobe.com>
2024-01-24 16:12:01 -0500

    Simplify moon script.

--- gallery/Moon/moon.py
+++ gallery/Moon/moon.py
@@ -11,21 +11,13 @@ config.sensor.location = [3, 0, 0]
 # Render with both color and bump map.
 base = hkw.layer("data/moon.ply").channel(
     material=hkw.material.Principled(
-        color=hkw.texture.Image(pathlib.Path("data/lroc_color_poles_8k.png")),
-        bump_map=hkw.texture.Image(pathlib.Path("data/ldem_16_uint.png")),
-        bump_scale=0.1,
-    )
+        color=hkw.texture.Image("data/lroc_color_poles_8k.png"),
+    ),
+    bump_map=hkw.channel.BumpMap(hkw.texture.Image("data/ldem_16_uint.png"), scale=0.1),
 )
 hkw.render(base, config, filename="results/moon.png")
 
 # Render back side.
 config.emitters[0].rotation = 90
 config.sensor.location = [-3, 0, 0]
-base = hkw.layer("data/moon.ply").channel(
-    material=hkw.material.Principled(
-        color=hkw.texture.Image(pathlib.Path("data/lroc_color_poles_8k.png")),
-        bump_map=hkw.texture.Image(pathlib.Path("data/ldem_16_uint.png")),
-        bump_scale=0.1,
-    )
-)
 hkw.render(base, config, filename="results/moon_backside.png")
