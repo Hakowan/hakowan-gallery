a74b2574eb5127fff05ce5b4816097cb22ded1c6
Qingnan Zhou <qzhou@adobe.com>
2024-03-18 19:30:22 -0400

    Update face example.

--- gallery/Face/face.py
+++ gallery/Face/face.py
@@ -10,7 +10,7 @@ config.sensor.location = [1.0, 0.5, 3]
 
 # Step 2: Render the 3D face mesh.
 face = hkw.layer("data/canonical_face_model.obj")
-hkw.render(face, config, filename="results/face.png")
+# hkw.render(face, config, filename="results/face.png")
 
 # Step 3: Render the 3D face mesh with checkerboard pattern.
 face_checkerboard = face.channel(
@@ -19,12 +19,12 @@ face_checkerboard = face.channel(
         roughness=0.1,
     )
 )
-hkw.render(face_checkerboard, config, filename="results/face_checkerboard.png")
+# hkw.render(face_checkerboard, config, filename="results/face_checkerboard.png")
 
 # Step 4: Render the UV mesh with 3D normal field.
 face_uv = (
-    face.transform(hkw.transform.UVMesh())
-    .transform(hkw.transform.Compute(vertex_normal="normal"))
+    face.transform(hkw.transform.Compute(vertex_normal="normal"))
+    .transform(hkw.transform.UVMesh())
     .channel(normal="normal", material=hkw.material.Diffuse("ivory"))
 )
 config.film.width = 1024
