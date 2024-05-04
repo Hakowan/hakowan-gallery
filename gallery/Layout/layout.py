4df3e14313720768e0c2cc550be76097e57fe97c
Qingnan Zhou <qzhou@adobe.com>
2024-05-04 16:52:08 -0400

    Update layout example.

--- gallery/Layout/layout.py
+++ gallery/Layout/layout.py
@@ -1,41 +1,10 @@
 #!/usr/bin/env python
 
 import hakowan as hkw
-import lagrange
-import numpy as np
+from data import load_data
 
 # Step 1: Preprocess data.
-
-# The ref mesh contains the base geometry.
-ref_mesh = lagrange.io.load_mesh("data/teaser_pig_bnb_target.obj")
-
-# Chain mesh contains the layout boundaries.
-# Each chain is a sequence of ref mesh vertices.
-chains_mesh = lagrange.SurfaceMesh()
-chains_mesh.add_vertices(ref_mesh.vertices)
-
-# Load chains from the .lem file.
-chains = []
-with open("data/teaser_pig_bnb.lem", "r") as fin:
-    for line in fin:
-        if line.startswith("ee"):
-            chain = line.split(":")[1].split()
-            chain = [int(i) for i in chain]
-            chains.append(chain)
-
-# Add chains to the chain mesh.
-for chain in chains:
-    for i in range(len(chain) - 1):
-        chains_mesh.add_polygon(np.array([chain[i], chain[i + 1]]))
-
-# Compute layout charts.
-involved_vertices = np.unique(np.hstack(chains)).tolist()
-lagrange.compute_components(
-    ref_mesh,
-    output_attribute_name="chart",
-    connectivity_type=lagrange.ConnectivityType.Vertex,
-    blocker_elements=involved_vertices,
-)
+ref_mesh, chains_mesh = load_data()
 
 # Step 2: Generate layout layers.
 base = hkw.layer(ref_mesh)
