53641f53adaa47cb704d2e23f80c7fa2d2ca2366
Qingnan Zhou <qzhou@adobe.com>
2026-09-24 17:48:56 -0400

    Update flow example (#3)

--- gallery/Flow/flow.py
+++ gallery/Flow/flow.py
@@ -6,8 +6,6 @@ import hakowan as hkw
 hkw.set_default_backend("webgl")
 
 flow0 = hkw.layer("data/flow_00.ply").name("Surface 0")
-flow2 = hkw.layer("data/flow_02.ply").name("Surface 2")
-flow5 = hkw.layer("data/flow_05.ply").name("Surface 5")
 flow9 = hkw.layer("data/flow_09.ply").name("Surface 9")
 
 s = 0.3
@@ -16,12 +14,12 @@ mat = hkw.material.Principled(
         hkw.attribute(
             "dist",
             scale=hkw.scale.Normalize(
-                domain_min=-s, domain_max=s, range_min=0, range_max=1
+                domain_min=0, domain_max=s, range_min=0, range_max=1
             ),
         ),
         legend=False,
     ),
-    roughness=0.5,
+    roughness=0.2,
     metallic=0.2,
 )
 
@@ -29,21 +27,28 @@ legend_mat = copy.deepcopy(mat)
 legend_mat.color.legend = hkw.Legend(title="Distance")
 
 dist0 = flow0.name("Distance 0").channel(material=legend_mat)
-dist2 = flow2.name("Distance 2").channel(material=copy.deepcopy(mat))
-dist5 = flow5.name("Distance 5").channel(material=copy.deepcopy(mat))
 dist9 = flow9.name("Distance 9").channel(material=copy.deepcopy(mat))
 
-comparison = (dist0 | dist2 | dist5 | dist9).compare(
-    flow0 | flow2 | flow5 | flow9,
-    axis="z",
-)
-scene = hkw.SceneSettings(
-    camera=hkw.PerspectiveCamera(eye=(0, -2.5, 0), up=(0, 0, 1)),
-    environment=hkw.Environment(up=(0, 0, 1)),
-    output=hkw.OutputSettings(width=600, height=1024),
-)
-RECIPE_FIGURE = hkw.Figure(comparison, scene)
-RECIPE_INSPECTIONS = {"surface": "data/flow_00.ply"}
+comp = flow0.compare(flow9, gap=-0.4, labels=["itr 0", "itr 9"])
+mix0 = hkw.grid([flow0, dist0], columns=2, column_gap=-0.4)
+mix9 = hkw.grid([flow9, dist9], columns=2, column_gap=-0.4)
+comp_all = hkw.grid([flow0, dist0, flow9, dist9], columns=4)
 
-hkw.render(RECIPE_FIGURE, backend="mitsuba", filename="results/bust.webp")
+scene = hkw.SceneSettings(
+    camera=hkw.PerspectiveCamera(eye=(0, -4, 0), up=(0, 0, 1)),
+    environment=hkw.Environment(up=(0, 0, 1)),
+    output=hkw.OutputSettings(width=1000, height=800),
+)
+RECIPE_FIGURE = hkw.Figure(comp_all, scene)
+RECIPE_INSPECTIONS = {
+    "initial-surface": "data/flow_00.ply",
+    "final-surface": "data/flow_09.ply",
+}
+
+hkw.render(
+    hkw.Figure(comp, scene), backend="mitsuba", filename="results/bust_comp.webp"
+)
+hkw.render(hkw.Figure(mix0, scene), backend="mitsuba", filename="results/bust_00.webp")
+hkw.render(hkw.Figure(mix9, scene), backend="mitsuba", filename="results/bust_09.webp")
+
 hkw.render(RECIPE_FIGURE, filename="results/bust_all.html")
